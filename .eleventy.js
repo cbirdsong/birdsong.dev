3d34721893e89250171d0141e2d41f1e43e5a81a
Cory Birdsong <cbirdsong@bbrcreative.com>
2020-03-06 11:48:35 -0600

    Add redirects

--- .eleventy.js
+++ .eleventy.js
@@ -5,107 +5,108 @@ const htmlmin = require("html-minifier");
 const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
 
 module.exports = function(eleventyConfig) {
+	// Eleventy Navigation https://www.11ty.dev/docs/plugins/navigation/
+	eleventyConfig.addPlugin(eleventyNavigationPlugin);
 
-  // Eleventy Navigation https://www.11ty.dev/docs/plugins/navigation/
-  eleventyConfig.addPlugin(eleventyNavigationPlugin);
+	eleventyConfig.addLayoutAlias("post", "layouts/post.njk");
 
-  eleventyConfig.addLayoutAlias("post", "layouts/post.njk");
+	// Date formatting (human readable)
+	eleventyConfig.addFilter("readableDate", dateObj => {
+		return DateTime.fromJSDate(dateObj).toFormat("dd LLL yyyy");
+	});
 
-  // Date formatting (human readable)
-  eleventyConfig.addFilter("readableDate", dateObj => {
-    return DateTime.fromJSDate(dateObj).toFormat("dd LLL yyyy");
-  });
+	// Date formatting (machine readable)
+	eleventyConfig.addFilter("machineDate", dateObj => {
+		return DateTime.fromJSDate(dateObj).toFormat("yyyy-MM-dd");
+	});
 
-  // Date formatting (machine readable)
-  eleventyConfig.addFilter("machineDate", dateObj => {
-    return DateTime.fromJSDate(dateObj).toFormat("yyyy-MM-dd");
-  });
+	// Minify CSS
+	eleventyConfig.addFilter("cssmin", function(code) {
+		return new CleanCSS({}).minify(code).styles;
+	});
 
-  // Minify CSS
-  eleventyConfig.addFilter("cssmin", function(code) {
-    return new CleanCSS({}).minify(code).styles;
-  });
+	// Minify JS
+	eleventyConfig.addFilter("jsmin", function(code) {
+		let minified = UglifyJS.minify(code);
+		if (minified.error) {
+			console.log("UglifyJS error: ", minified.error);
+			return code;
+		}
+		return minified.code;
+	});
 
-  // Minify JS
-  eleventyConfig.addFilter("jsmin", function(code) {
-    let minified = UglifyJS.minify(code);
-    if (minified.error) {
-      console.log("UglifyJS error: ", minified.error);
-      return code;
-    }
-    return minified.code;
-  });
+	// Minify HTML output
+	eleventyConfig.addTransform("htmlmin", function(content, outputPath) {
+		if (outputPath.indexOf(".html") > -1) {
+			let minified = htmlmin.minify(content, {
+				useShortDoctype: true,
+				removeComments: true,
+				collapseWhitespace: true
+			});
+			return minified;
+		}
+		return content;
+	});
 
-  // Minify HTML output
-  eleventyConfig.addTransform("htmlmin", function(content, outputPath) {
-    if (outputPath.indexOf(".html") > -1) {
-      let minified = htmlmin.minify(content, {
-        useShortDoctype: true,
-        removeComments: true,
-        collapseWhitespace: true
-      });
-      return minified;
-    }
-    return content;
-  });
+	// only content in the `posts/` directory
+	eleventyConfig.addCollection("posts", function(collection) {
+		return collection.getAllSorted().filter(function(item) {
+			return item.inputPath.match(/^\.\/posts\//) !== null;
+		});
+	});
 
-  // only content in the `posts/` directory
-  eleventyConfig.addCollection("posts", function(collection) {
-    return collection.getAllSorted().filter(function(item) {
-      return item.inputPath.match(/^\.\/posts\//) !== null;
-    });
-  });
+	// Universal slug filter strips unsafe chars from URLs
+	eleventyConfig.addFilter("slugify", function(str) {
+		return slugify(str, {
+			lower: true,
+			replacement: "-",
+			remove: /[*+~.·,()'"`´%!?¿:@]/g
+		});
+	});
 
-  // Universal slug filter strips unsafe chars from URLs
-  eleventyConfig.addFilter("slugify", function(str) {
-    return slugify(str, {
-      lower: true,
-      replacement: "-",
-      remove: /[*+~.·,()'"`´%!?¿:@]/g
-    });
-  });
+	// Don't process folders with static assets e.g. images
+	eleventyConfig.addPassthroughCopy("_redirects");
+	eleventyConfig.addPassthroughCopy("favicon.ico");
+	eleventyConfig.addPassthroughCopy("static/img");
+	eleventyConfig.addPassthroughCopy("static/fonts");
+	eleventyConfig.addPassthroughCopy("admin");
+	eleventyConfig.addPassthroughCopy("_includes/assets/");
 
-  // Don't process folders with static assets e.g. images
-  eleventyConfig.addPassthroughCopy("favicon.ico");
-  eleventyConfig.addPassthroughCopy("static/img");
-  eleventyConfig.addPassthroughCopy("static/fonts");
-  eleventyConfig.addPassthroughCopy("admin");
-  eleventyConfig.addPassthroughCopy("_includes/assets/");
+	/* Markdown Plugins */
+	let markdownIt = require("markdown-it");
+	let markdownItAnchor = require("markdown-it-anchor");
+	let options = {
+		html: true,
+		breaks: true,
+		linkify: true
+	};
+	let opts = {
+		permalink: false
+	};
 
-  /* Markdown Plugins */
-  let markdownIt = require("markdown-it");
-  let markdownItAnchor = require("markdown-it-anchor");
-  let options = {
-    html: true,
-    breaks: true,
-    linkify: true
-  };
-  let opts = {
-    permalink: false
-  };
+	eleventyConfig.setLibrary(
+		"md",
+		markdownIt(options).use(markdownItAnchor, opts)
+	);
 
-  eleventyConfig.setLibrary("md", markdownIt(options)
-    .use(markdownItAnchor, opts)
-  );
+	return {
+		templateFormats: ["md", "njk", "html", "liquid"],
 
-  return {
-    templateFormats: ["md", "njk", "html", "liquid"],
+		// If your site lives in a different subdirectory, change this.
+		// Leading or trailing slashes are all normalized away, so don’t worry about it.
+		// If you don’t have a subdirectory, use "" or "/" (they do the same thing)
+		// This is only used for URLs (it does not affect your file structure)
+		pathPrefix: "/",
 
-    // If your site lives in a different subdirectory, change this.
-    // Leading or trailing slashes are all normalized away, so don’t worry about it.
-    // If you don’t have a subdirectory, use "" or "/" (they do the same thing)
-    // This is only used for URLs (it does not affect your file structure)
-    pathPrefix: "/",
-
-    markdownTemplateEngine: "liquid",
-    htmlTemplateEngine: "njk",
-    dataTemplateEngine: "njk",
-    passthroughFileCopy: true,
-    dir: {
-      input: ".",
-      includes: "_includes",
-      data: "_data",
-      output: "_site"
-    }
-  };
+		markdownTemplateEngine: "liquid",
+		htmlTemplateEngine: "njk",
+		dataTemplateEngine: "njk",
+		passthroughFileCopy: true,
+		dir: {
+			input: ".",
+			includes: "_includes",
+			data: "_data",
+			output: "_site"
+		}
+	};
 };
