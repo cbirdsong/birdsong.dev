4e926bee1ffeb157a24d3d6da1e8cfa89e1960ac
Cory Birdsong <cory@birdsong.dev>
2022-01-14 12:26:16 -0600

    Various content/design tweaks

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,5 @@
 const { DateTime } = require("luxon");
 const CleanCSS = require("clean-css");
-const UglifyJS = require("uglify-es");
-const htmlmin = require("html-minifier");
 const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
 
 module.exports = function (eleventyConfig) {
@@ -27,29 +25,6 @@ module.exports = function (eleventyConfig) {
 	// Minify CSS
 	eleventyConfig.addFilter("cssmin", function (code) {
 		return new CleanCSS({}).minify(code).styles;
-	});
-
-	// Minify JS
-	eleventyConfig.addFilter("jsmin", function (code) {
-		let minified = UglifyJS.minify(code);
-		if (minified.error) {
-			console.log("UglifyJS error: ", minified.error);
-			return code;
-		}
-		return minified.code;
-	});
-
-	// Minify HTML output
-	eleventyConfig.addTransform("htmlmin", function (content, outputPath) {
-		if (outputPath.indexOf(".html") > -1) {
-			let minified = htmlmin.minify(content, {
-				useShortDoctype: true,
-				removeComments: true,
-				collapseWhitespace: true,
-			});
-			return minified;
-		}
-		return content;
 	});
 
 	// Add posts collection
